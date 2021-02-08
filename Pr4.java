fa524bebe42cbdad66d96ee6d44e02df953ee18c
UttamBindal <ping.uttambindal@gamil.com>
2021-02-09 00:09:25 +0530

    Basic Program to take three numbers as input and check whether the number generated by adding the first two and multiplying it with the third is greater than 100 or not

---
+++ Pr4.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class Pr4
+{
+    public static void main (String agrs[]) throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter 3 Numbers");
+        System.out.print("a=");
+        int a=Integer.parseInt(in.readLine());
+        System.out.print("b=");
+        int b=Integer.parseInt(in.readLine());
+        System.out.print("c=");
+        int c=Integer.parseInt(in.readLine());
+        int re=(a+b)*c;
+        if(re<=100)
+        System.out.println("Greater than 100");
+        else
+        System.out.println("Less Than 100");
+    }
+}
+
